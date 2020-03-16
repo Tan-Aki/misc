373f7755c3f8e87b495d27b87f22d28bf593d7e7
TanSoloo <tanneguy.jullin@gmail.com>
2020-03-16 09:12:56 -0400

    readme modified for Advanced CSS class

--- React-complete-guide/react-complete-guide/src/index.js
+++ React-complete-guide/react-complete-guide/src/index.js
@@ -6,3 +6,12 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+// 1. Functional components (also referred to as "presentational", "dumb" or
+// "stateless" components - more about this later in the course) =>
+// const cmp = () => { return <div>some JSX</div> }
+// (using ES6 arrow functions as shown here is recommended but optional)
+
+// 2. class-based components (also referred to as "containers", "smart" or "stateful"
+// components) =>
+// class Cmp extends Component { render () { return <div>some JSX</div> } }
