b3223dbf9e9ffb9234be17b0303ca1b95bf033e9
TanSoloo <tanneguy.jullin@gmail.com>
2020-03-25 19:27:21 -0400

    styling react components

---
+++ React-complete-guide/react-basics/src/Button/button.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Btn } from './button.module.scss'
+
+
+
+const button = props => {
+
+
+    return (
+        <div className={Btn}>
+            <button>Click ! </button>
+        </div>
+    );
+};
+
+export default button;
