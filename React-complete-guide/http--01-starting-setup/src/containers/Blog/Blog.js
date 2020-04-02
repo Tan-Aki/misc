9eb3f440e4a278fcdc1a47951f01d2ec01cb234a
TanSoloo <tanneguy.jullin@gmail.com>
2020-04-02 19:21:41 -0400

    http and ajax

---
+++ React-complete-guide/http--01-starting-setup/src/containers/Blog/Blog.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+
+class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null
+  };
+
+  async componentDidMount() {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    console.log(response);
+    const posts = response.data.slice(0, 4);
+    const updatedPosts = posts.map(post => {
+      return {
+        ...post,
+        author: "Tan"
+      };
+    });
+    this.setState({ posts: updatedPosts });
+  }
+
+  postSelectedHandler = id => {
+    // const selectedPost = this.state.posts.find(post => post.id === id);
+    // console.log(selectedPost);
+    this.setState({ selectedPostId: id });
+  };
+
+  render() {
+    const posts = this.state.posts.map(post => {
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
+    });
+
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Blog;
