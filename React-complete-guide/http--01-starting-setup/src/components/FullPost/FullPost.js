ee6d61219980a8d983dbc9e110b40cd88151d065
TanSoloo <tanneguy.jullin@gmail.com>
2020-04-03 15:55:25 -0400

    http and ajax requests module completed

--- React-complete-guide/http--01-starting-setup/src/components/FullPost/FullPost.js
+++ React-complete-guide/http--01-starting-setup/src/components/FullPost/FullPost.js
@@ -10,12 +10,15 @@ class FullPost extends Component {
 
   async componentDidUpdate(prevProps, prevState) {
     if (/*this.props.id && */ prevProps.id !== this.props.id) {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts/" + this.props.id
-      );
+      const response = await axios.get("/posts/" + this.props.id);
       this.setState({ loadedPost: response.data });
     }
   }
+
+  deletePostHandler = async () => {
+    const response = await axios.delete("/posts/" + this.props.id);
+    console.log(response);
+  };
 
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
@@ -28,7 +31,9 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
-            <button className="Delete">Delete</button>
+            <button className="Delete" onClick={this.deletePostHandler}>
+              Delete
+            </button>
           </div>
         </div>
       );
--- React-complete-guide/http--01-starting-setup/src/index.js
+++ React-complete-guide/http--01-starting-setup/src/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+// default axios settings being set globally inside index.js
+// will be overridden by instance settings
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+axios.interceptors.request.use(
+  request => {
+    console.log("request : ", request);
+    //Edit request
+    return request;
+  },
+  error => {
+    console.log("error :", error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.request.use(
+  response => {
+    console.log("response : ", response);
+    //Edit response
+    return response;
+  },
+  error => {
+    console.log("error :", error);
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
