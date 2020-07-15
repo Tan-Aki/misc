faf9f30a0e0a36119045f4d70a8c6c78089687ca
Tan-Aki <tanneguy.jullin@gmail.com>
2020-07-15 12:35:46 -0400

    React animations tuto

--- React/Tutos/animations/src/components/List/List.js
+++ React/Tutos/animations/src/components/List/List.js
@@ -1,48 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import TransitionGroup from "react-transition-group/TransitionGroup";
+import CSSTransition from "react-transition-group/CSSTransition";
 
-import classes from './List.module.scss';
-
-
+import classes from "./List.module.scss";
 
 class List extends Component {
-    state = {
-        items: [1, 2, 3]
-    }
+  state = {
+    items: [1, 2, 3],
+  };
 
-    addItemHandler = () => {
-        this.setState((prevState) => {
-            return {
-                items: prevState.items.concat(prevState.items.length + 1)
-            };
-        });
-    }
+  addItemHandler = () => {
+    this.setState((prevState) => {
+      return {
+        items: prevState.items.concat(prevState.items.length + 1),
+      };
+    });
+  };
 
-    removeItemHandler = (selIndex) => {
-        this.setState((prevState) => {
-            return {
-                items: prevState.items.filter((item, index) => index !== selIndex)
-            };
-        });
-    }
+  removeItemHandler = (selIndex) => {
+    this.setState((prevState) => {
+      return {
+        items: prevState.items.filter((item, index) => index !== selIndex),
+      };
+    });
+  };
 
-    render () {
-        const listItems = this.state.items.map( (item, index) => (
-            <li 
-                key={index}
-                className={classes.ListItem} 
-                onClick={() => this.removeItemHandler(index)}>{item}</li>
-        ) );
+  render() {
+    const listItems = this.state.items.map((item, index) => (
+      <CSSTransition
+        key={index}
+        classNames={{
+          enter: classes.FadeEnter,
+          enterActive: classes.FadeEnterActive,
+          exit: classes.FadeExit,
+          exitActive: classes.FadeExitActive,
 
-        return (
-            <div>
-                <button className="Button" onClick={this.addItemHandler}>Add Item</button>
-                <p>Click Item to Remove.</p>
-                <ul className={classes.List}>
+          // appear: classes["my-appear"],
+          // appearActive: classes["my-active-appear"],
+          // appearDone: classes["my-done-appear"],
+          // enter: classes["my-enter"],
+          // enterDone: classes["my-done-enter"],
+          // exit: classes["my-exit"],
+          // exitDone: classes["my-done-exit"],
+        }}
+        timeout={300}
+      >
+        <li className={classes.ListItem} onClick={() => this.removeItemHandler(index)}>
+          {item}
+        </li>
+      </CSSTransition>
+    ));
+
+    return (
+      <div>
+        <button className="Button" onClick={this.addItemHandler}>
+          Add Item
+        </button>
+        <p>Click Item to Remove.</p>
+        {/* <ul className={classes.List}>
                     {listItems}
-                </ul>
-            </div>
-        );
-    }
+                </ul> */}
+        <TransitionGroup className={classes.List} component="ul">
+            {/* TransitionGroup will manage the "in" property on  each CSSTransition item*/}
+          {listItems}
+        </TransitionGroup>
+      </div>
+    );
+  }
 }
 
 export default List;
--- React/Tutos/animations/src/components/Modal/Modal.js
+++ React/Tutos/animations/src/components/Modal/Modal.js
@@ -2,10 +2,9 @@ import React from "react";
 import classNames from "classnames/bind";
 
 import classes from "./Modal.module.scss";
-import Transition from "react-transition-group/Transition";
+import CSSTransition from "react-transition-group/CSSTransition";
 
-
-// The 4 states 
+// The 4 states
 // 'entering'
 // 'entered'
 // 'exiting'
@@ -16,30 +15,91 @@ const animationTiming = {
   exit: 1000, // duration for removing the element, from exiting to exited
 };
 
+const cx = classNames.bind(classes);
+
+const modalClass = cx({
+  Modal: true,
+});
+
 const modal = (props) => {
   return (
-    <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
-      {(state) => {
-        const cx = classNames.bind(classes);
-        const modalClass = cx({
-          Modal: true,
-          ModalOpen: state === "entering",
-          ModalClosed: state === "exiting",
-        });
+    <CSSTransition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      classNames={{
+        enterActive: classes.ModalOpen,
+        exitActive: classes.ModalClosed,
 
-        return (
-          <div className={modalClass}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
+        // enterActive: classes["fade-slide-enter-active"],
+        // exitActive: classes["fade-slide-exit-active"],
+
+        // appear: classes["my-appear"],
+        // appearActive: classes["my-active-appear"],
+        // appearDone: classes["my-done-appear"],
+        // enter: classes["my-enter"],
+        // enterDone: classes["my-done-enter"],
+        // exit: classes["my-exit"],
+        // exitDone: classes["my-done-exit"],
       }}
-    </Transition>
+    >
+      <div className={modalClass}>
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 export default modal;
+
+///// modal opened and closed with css and Transitions, disappears from HTML when closed, and appears when opened
+
+// import React from "react";
+// import classNames from "classnames/bind";
+
+// import classes from "./Modal.module.scss";
+// import Transition from "react-transition-group/Transition";
+
+// // The 4 states
+// // 'entering'
+// // 'entered'
+// // 'exiting'
+// // 'exited'
+
+// const animationTiming = {
+//   enter: 400, // duration for adding the element, from entering to entered
+//   exit: 1000, // duration for removing the element, from exiting to exited
+// };
+
+// const cx = classNames.bind(classes);
+
+// const modal = (props) => {
+//   return (
+//     <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
+//       {(state) => {
+//
+//         const modalClass = cx({
+//           Modal: true,
+//           ModalOpen: state === "entering",
+//           ModalClosed: state === "exiting",
+//         });
+
+//         return (
+//           <div className={modalClass}>
+//             <h1>A Modal</h1>
+//             <button className="Button" onClick={props.closed}>
+//               Dismiss
+//             </button>
+//           </div>
+//         );
+//       }}
+//     </Transition>
+//   );
+// };
+// export default modal;
 
 ///// modal opened and closed with css, always present in HTML
 
