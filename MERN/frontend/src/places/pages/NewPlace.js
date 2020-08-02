6386bf7c7dc737045cf6a11cb8c2dd349282249f
Tan-Aki <tanneguy.jullin@gmail.com>
2020-08-02 16:51:54 -0400

    MERN : Form validation done

--- MERN/frontend/src/places/pages/NewPlace.js
+++ MERN/frontend/src/places/pages/NewPlace.js
@@ -1,19 +1,78 @@
-import React from "react";
+import React, { useCallback, useReducer } from "react";
 
 import "./NewPlace.css";
+import Button from "../../shared/components/FormElements/Button";
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from "../../shared/util/validators";
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
 
 const NewPlace = (props) => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({ type: "INPUT_CHANGE", value: value, isValid: isValid, inputId: id });
+  }, []);
+  //useCallback to prevent infinite loop ( in relation with the useEffect in Input.js file and the fact that if we change the state of this NewPlace.js component in this function,
+  // it will cause a re-render of this component, and therefore  a re-render of this function, and therefore the useEffect in Input.js will trigger again, causing an infi loop)
+  // with useCallback this function will not be recreated everytime the component is re-renderered anymore, therefor we break the inifinite loop
+
   return (
     <form className="place-form">
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
+        onInput={inputHandler}
       />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={inputHandler}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
     </form>
   );
 };
