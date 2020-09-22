66bcc7b469d41380ad0e0013d28f85482297e8d6
Tan-Aki <tanneguy.jullin@gmail.com>
2020-09-22 18:13:57 -0400

    MERN : clean up test folder since they are on the testing branch

--- MERN/App/backend/test/hellotest.test.js
+++
@@ -1,6 +0,0 @@
-const assert = require('assert');
-describe('Basic Mocha String Test', function () {
-    it('should return number of characters is 5', function () {
-        assert.strictEqual('Hello'.length, 5);
-    });
-});
