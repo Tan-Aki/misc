2cfb1073f514fe211e693f0f0e59779711f71157
Tan-Aki <tanneguy.jullin@gmail.com>
2020-09-23 19:49:49 -0400

    jest-testing : React Testing project in progress

---
+++ Testing/jest-testing/src/App.test.js
@@ -0,0 +1,197 @@
+/////////////////////////////////////////////////////////////////////////////
+//// With basic queries in react dom
+/////////////////////////////////////////////////////////////////////////////
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// it('renders the correct content', () => {
+//     const root = document.createElement('div');
+//     ReactDOM.render(<App />, root);
+
+//     expect(root.querySelector('h1').textContent).toBe('TODOs');
+//     expect(root.querySelector('label').textContent).toBe('What needs to be done?');
+//     expect(root.querySelector('button').textContent).toBe('Add #1');
+// });
+
+//// with testing library
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import { getQueriesForElement } from '@testing-library/dom';
+
+// it('renders the correct content', () => {
+//     const root = document.createElement('div');
+//     ReactDOM.render(<App />, root);
+
+//     const { getByText, getByLabelText } = getQueriesForElement(root);
+
+//     // expect(getByText('TODOs')).not.toBeNull();
+//     // expect(getByLabelText('What needs to be done?')).not.toBeNull();
+//     // expect(getByText('Add #1')).not.toBeNull();
+
+//     // leaner version
+//     getByText('TODOs');
+//     getByLabelText('What needs to be done?');
+//     getByText('Add #1');
+// });
+
+/////////////////////////////////////////////////////////////////////////////
+//// with testing library and abstract render
+/////////////////////////////////////////////////////////////////////////////
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import { getQueriesForElement } from '@testing-library/dom';
+
+// const render = (component) => {
+//     const root = document.createElement('div');
+//     ReactDOM.render(component, root);
+//     return getQueriesForElement(root);
+// };
+
+// it('renders the correct content', () => {
+//     const { getByText, getByLabelText } = render(<App />);
+
+//     // expect(getByText('TODOs')).not.toBeNull();
+//     // expect(getByLabelText('What needs to be done?')).not.toBeNull();
+//     // expect(getByText('Add #1')).not.toBeNull();
+
+//     // leaner version
+//     getByText('TODOs');
+//     getByLabelText('What needs to be done?');
+//     getByText('Add #1');
+// });
+
+/////////////////////////////////////////////////////////////////////////////
+//// with react testing library
+/////////////////////////////////////////////////////////////////////////////
+
+// import React from 'react';
+// import { render } from '@testing-library/react';
+
+// import App from './App';
+
+// it('renders the correct content', () => {
+//     const { getByText, getByLabelText } = render(<App />);
+
+//     // expect(getByText('TODOs')).not.toBeNull();
+//     // expect(getByLabelText('What needs to be done?')).not.toBeNull();
+//     // expect(getByText('Add #1')).not.toBeNull();
+
+//     // leaner version
+//     getByText('TODOs');
+//     getByLabelText('What needs to be done?');
+//     getByText('Add #1');
+// });
+
+/////////////////////////////////////////////////////////////////////////////
+////  Simulating user interaction with fireEvent
+//// before, you would use enzime, and get a wrapper object, and access the state of that wrapper object
+//// expect(wrapper.state("items")).toBe(["First task"])
+/////////////////////////////////////////////////////////////////////////////
+
+// import React from 'react';
+// import { render, fireEvent } from '@testing-library/react';
+
+// import App from './App';
+
+// it('renders the correct content', () => {
+//     const { getByText, getByLabelText } = render(<App />);
+
+//     // expect(getByText('TODOs')).not.toBeNull();
+//     // expect(getByLabelText('What needs to be done?')).not.toBeNull();
+//     // expect(getByText('Add #1')).not.toBeNull();
+
+//     // leaner version
+//     getByText('TODOs');
+//     getByLabelText('What needs to be done?');
+//     getByText('Add #1');
+// });
+
+// // "it" or "test" does the same , just a different keyword
+// test('allows users to add items to their list', () => {
+//     const { getByText, getByLabelText } = render(<App />);
+
+//     const input = getByLabelText('What needs to be done?');
+
+//     fireEvent.change(input, { target: { value: 'First task' } });
+//     fireEvent.click(getByText('Add #1'));
+
+//     getByText('First task');
+//     getByText('Add #2');
+// });
+
+/////////////////////////////////////////////////////////////////////////////
+////  Simulating user interaction with user-event
+/////////////////////////////////////////////////////////////////////////////
+
+// import React from 'react';
+// import { render, fireEvent } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+
+// import App from './App';
+
+// it('renders the correct content', () => {
+//     const { getByText, getByLabelText } = render(<App />);
+
+//     // expect(getByText('TODOs')).not.toBeNull();
+//     // expect(getByLabelText('What needs to be done?')).not.toBeNull();
+//     // expect(getByText('Add #1')).not.toBeNull();
+
+//     // leaner version
+//     getByText('TODOs');
+//     getByLabelText('What needs to be done?');
+//     getByText('Add #1');
+// });
+
+// // "it" or "test" does the same , just a different keyword
+// test('allows users to add items to their list', () => {
+//     const { getByText, getByLabelText } = render(<App />);
+
+//     const input = getByLabelText('What needs to be done?');
+
+//     // fireEvent.change(input, { target: { value: 'First task' } });
+//     userEvent.type(input, 'First task');
+//     // with the userEvent.type you can specify the number of milliseconds to wait in between key strokes
+//     // options.delay is the number of milliseconds that pass between two characters are typed. By default it's 0. You can use this option if your component has a different behavior for fast or slow users. If you do this, you need to make sure to await!
+//     fireEvent.click(getByText('Add #1'));
+
+//     getByText('First task');
+//     getByText('Add #2');
+// });
+
+
+/////////////////////////////////////////////////////////////////////////////
+////  testing async code  // TO DO
+/////////////////////////////////////////////////////////////////////////////
+
+
+// import React from 'react';
+// import { render, fireEvent } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+
+// import App from './App';
+
+// it('renders the correct content', () => {
+//     const { getByText, getByLabelText } = render(<App />);
+
+//     getByText('TODOs');
+//     getByLabelText('What needs to be done?');
+//     getByText('Add #1');
+// });
+
+// test('allows users to add items to their list', () => {
+//     const { getByText, getByLabelText } = render(<App />);
+
+//     const input = getByLabelText('What needs to be done?');
+
+//     userEvent.type(input, 'First task');
+//     fireEvent.click(getByText('Add #1'));
+
+//     getByText('First task');
+//     getByText('Add #2');
+// });
