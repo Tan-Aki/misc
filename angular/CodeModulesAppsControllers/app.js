cb49e61e7df744b51d32f907c4cd9b27bb7f8f3c
Tanneguy Jullin <t.jullin@mphase.ca>
2020-10-28 17:15:18 -0400

    Angular : other services

--- angular/CodeModulesAppsControllers/app.js
+++ angular/CodeModulesAppsControllers/app.js
@@ -25,25 +25,47 @@
 // // logPerson();
 
 ////////////////////////////////////////////////////////////////////////////
-//////////                                    //////////////////////////////
+//////////     dependency Injection and scope service         //////////////
 ////////////////////////////////////////////////////////////////////////////
 
+// // MODULE
+// var angularApp = angular.module('angularApp', []);
+
+// // CONTROLLERS
+// angularApp.controller('mainController', ['$scope', function ($scope) {}]);
+
+// var searchPeople = function (firstName, lastName, height, age, occupation) {
+//     return 'Janes Doe';
+// };
+
+// console.log(searchPeople(1, 2, 3, 4, 5)); // Janes Doe
+
+// console.log(searchPeople); // ƒ (firstName, lastName, height, age, occupation) {return 'Janes Doe';}
+
+// var searchPeopleString = searchPeople.toString();
+
+// console.log(searchPeopleString); // function (firstName, lastName, height, age, occupation) {return 'Janes Doe';}
+
+// console.log(angular.injector().annotate(searchPeople)) //["firstName", "lastName", "height", "age", "occupation"]
+
+////////////////////////////////////////////////////////////////////////////
+//////////      Other Services                            ///////////
+////////////////////////////////////////////////////////////////////////////
 // MODULE
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', function ($scope) {}]);
+angularApp.controller('mainController', [
+    '$scope',
+    '$log',
+    function ($scope, $log) {
+        console.log($log);
+        $log.log("hello !")
+        $log.info("info !")
+        $log.warn("warn !")
+        $log.debug("debug !")
+        $log.error("error !")
 
-var searchPeople = function (firstName, lastName, height, age, occupation) {
-    return 'Janes Doe';
-};
 
-console.log(searchPeople(1, 2, 3, 4, 5)); // Janes Doe
-
-console.log(searchPeople); // ƒ (firstName, lastName, height, age, occupation) {return 'Janes Doe';}
-
-var searchPeopleString = searchPeople.toString();
-
-console.log(searchPeopleString); // function (firstName, lastName, height, age, occupation) {return 'Janes Doe';}
-
-console.log(angular.injector().annotate(searchPeople)) //["firstName", "lastName", "height", "age", "occupation"]
+    },
+]);
