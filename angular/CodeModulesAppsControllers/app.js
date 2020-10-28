dd3e1ec503e82ff408cfe0a480bab4718a2cd15a
Tanneguy Jullin <t.jullin@mphase.ca>
2020-10-28 17:05:43 -0400

    $scope and injections

--- angular/CodeModulesAppsControllers/app.js
+++ angular/CodeModulesAppsControllers/app.js
@@ -1,9 +1,49 @@
+////////////////////////////////////////////////////////////////////////////
+////////// You can add variables to the scope //////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+// // MODULE
+// // person = 'tony';
+// var angularApp = angular.module('angularApp', []);
+
+// // CONTROLLERS
+// angularApp.controller('mainController', [
+//     '$scope',
+//     function ($scope) {
+//         $scope.name = 'Jane Doe';
+//         $scope.occupation = 'Coder';
+//         $scope.getName = function () {
+//             console.log('John Doe')
+//         };
+
+//         $scope.getName()
+
+//         console.log($scope);
+//     },
+// ]);
+
+// // logPerson();
+
+////////////////////////////////////////////////////////////////////////////
+//////////                                    //////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
 // MODULE
-person = 'tony';
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
 angularApp.controller('mainController', ['$scope', function ($scope) {}]);
 
-logPerson();
+var searchPeople = function (firstName, lastName, height, age, occupation) {
+    return 'Janes Doe';
+};
 
+console.log(searchPeople(1, 2, 3, 4, 5)); // Janes Doe
+
+console.log(searchPeople); // ƒ (firstName, lastName, height, age, occupation) {return 'Janes Doe';}
+
+var searchPeopleString = searchPeople.toString();
+
+console.log(searchPeopleString); // function (firstName, lastName, height, age, occupation) {return 'Janes Doe';}
+
+console.log(angular.injector().annotate(searchPeople)) //["firstName", "lastName", "height", "age", "occupation"]
