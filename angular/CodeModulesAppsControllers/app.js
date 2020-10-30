00eebf9c3017b17b208fead0ce5a7d7d98b26a58
Tanneguy Jullin <t.jullin@mphase.ca>
2020-10-30 16:23:25 -0400

    dependency injection and scope service and other services

--- angular/CodeModulesAppsControllers/app.js
+++ angular/CodeModulesAppsControllers/app.js
@@ -49,22 +49,75 @@
 // console.log(angular.injector().annotate(searchPeople)) //["firstName", "lastName", "height", "age", "occupation"]
 
 ////////////////////////////////////////////////////////////////////////////
-//////////      Other Services                            ///////////
+//////////      log Services                            ///////////
+////////////////////////////////////////////////////////////////////////////
+// // MODULE
+// var angularApp = angular.module('angularApp', []);
+
+// // CONTROLLERS
+// angularApp.controller('mainController', [
+//     '$scope',
+//     '$log',
+//     function ($scope, $log) {
+//         console.log($log);
+//         $log.log("hello !")
+//         $log.info("info !")
+//         $log.warn("warn !")
+//         $log.debug("debug !")
+//         $log.error("error !")
+
+
+//     },
+// ]);
+
+
+////////////////////////////////////////////////////////////////////////////
+//////////      other Services                            ///////////
+////////////////////////////////////////////////////////////////////////////
+// // MODULE
+// var angularApp = angular.module('angularApp', []);
+
+// // CONTROLLERS
+// angularApp.controller('mainController', [
+//     '$scope',
+//     '$log',
+//     '$filter',
+//     function ($scope, $log, $filter) {
+
+
+//         $scope.name = "John"
+//         $scope.formattedName = $filter('uppercase')($scope.name)
+
+//         $log.log($scope.name)
+//         $log.log($scope.formattedName)
+
+
+
+
+//     },
+// ]);
+
+////////////////////////////////////////////////////////////////////////////
+//////////      other Services not included by default in angular but that work with it ///////////
 ////////////////////////////////////////////////////////////////////////////
 // MODULE
-var angularApp = angular.module('angularApp', []);
+var angularApp = angular.module('angularApp', ['ngMessages']);
 
 // CONTROLLERS
 angularApp.controller('mainController', [
     '$scope',
     '$log',
-    function ($scope, $log) {
-        console.log($log);
-        $log.log("hello !")
-        $log.info("info !")
-        $log.warn("warn !")
-        $log.debug("debug !")
-        $log.error("error !")
+    '$filter',
+    function ($scope, $log, $filter) {
+
+
+        $scope.name = "John"
+        $scope.formattedName = $filter('uppercase')($scope.name)
+
+        $log.log($scope.name)
+        $log.log($scope.formattedName)
+
+
 
 
     },
