6d60fe8c662e02614e82b7c409f392ca2a7db1b0
Tan-Aki <tanneguy.jullin@gmail.com>
2023-07-06 22:09:29 -0400

    [Typescript] added sub and mult to first project

--- typescript/first-project/using-ts.js
+++ typescript/first-project/using-ts.js
@@ -1,4 +1,6 @@
-var button = document.querySelector('button');
+var button = document.querySelectorAll('button')[0];
+var buttonSub = document.querySelectorAll('button')[1];
+var buttonMul = document.querySelectorAll('button')[2];
 var input1 = document.getElementById('num1');
 var input2 = document.getElementById('num2');
 function add(num1, num2) {
@@ -6,4 +8,18 @@ function add(num1, num2) {
 }
 button.addEventListener('click', function () {
     console.log(add(+input1.value, +input2.value));
+});
+var substract = function (num1, num2) {
+    return num1 - num2;
+};
+// multiply function
+var multiply = function (num1, num2) {
+    return num1 * num2;
+};
+// add multiply and substract functions to their respective buttons
+buttonSub.addEventListener('click', function () {
+    console.log(substract(+input1.value, +input2.value));
+});
+buttonMul.addEventListener('click', function () {
+    console.log(multiply(+input1.value, +input2.value));
 });
--- typescript/first-project/using-ts.ts
+++ typescript/first-project/using-ts.ts
@@ -1,4 +1,7 @@
-const button = document.querySelector('button');
+const button = document.querySelectorAll('button')[0];
+const buttonSub = document.querySelectorAll('button')![1];
+const buttonMul = document.querySelectorAll('button')![2];
+
 const input1 = <HTMLInputElement>document.getElementById('num1');
 const input2 = <HTMLInputElement>document.getElementById('num2');
 
@@ -8,4 +11,22 @@ function add(num1: number, num2: number) {
 
 button!.addEventListener('click', function () {
     console.log(add(+input1!.value, +input2!.value));
+});
+
+const substract = (num1: number, num2: number) => {
+    return num1 - num2;
+};
+
+// multiply function
+const multiply = (num1: number, num2: number) => {
+    return num1 * num2;
+};
+
+// add multiply and substract functions to their respective buttons
+buttonSub!.addEventListener('click', function () {
+    console.log(substract(+input1!.value, +input2!.value));
+});
+
+buttonMul!.addEventListener('click', function () {
+    console.log(multiply(+input1!.value, +input2!.value));
 });
