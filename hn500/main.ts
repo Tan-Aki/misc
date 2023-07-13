72d2936d5c0351fdafb675869db6cd93a1c12ff1
Tan-Aki <tanneguy.jullin@gmail.com>
2023-07-13 18:15:38 -0400

    [hn500] Added env file; Added amazon ses

--- hn500/main.ts
+++ hn500/main.ts
@@ -1,4 +1,14 @@
 import axios from 'axios';
+import AWS from 'aws-sdk';
+import 'dotenv/config';
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+});
+
+const ses = new AWS.SES({ apiVersion: '2010-12-01' });
 
 const HN_API = 'https://hacker-news.firebaseio.com/v0';
 
@@ -18,12 +28,13 @@ const getBestStoriesWithMinScore = async (minScore: number) => {
 
     for (const id of bestStoryIds) {
         const story = await getStory(id);
+        console.log(`${story.title} - ${story.score} points`);
 
         if (story.score >= minScore) {
-            // Only include the title and URL in the output
             bestStories.push({
                 title: story.title,
                 url: story.url,
+                score: story.score,
             });
         }
     }
@@ -31,16 +42,40 @@ const getBestStoriesWithMinScore = async (minScore: number) => {
     return bestStories;
 };
 
-const formatStoriesForEmail = (stories: { title: string; url: string }[]) => {
-    return stories.map((story) => `<a href="${story.url}">${story.title}</a>`).join('<br>');
+const formatStoriesForEmail = (stories: { title: string; url: string; score: string }[]) => {
+    return stories.map((story) => `<a href="${story.url}">${story.title} - ${story.score} points </a>`).join('<br>');
 };
 
 const run = async () => {
     const stories = await getBestStoriesWithMinScore(500);
 
-    const emailContent = formatStoriesForEmail(stories);
+    const formattedStories = formatStoriesForEmail(stories);
 
-    console.log(emailContent);
+    const emailContent = `<h1>HackerNews Best Stories</h1><br><p>Here are the best stories on HackerNews with at least 500 points.</p><br><hr><br>` + formattedStories;
+
+    var params = {
+        Destination: {
+            ToAddresses: ['tanneguy.jullin@gmail.com'],
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: 'UTF-8',
+                    Data: emailContent,
+                },
+            },
+            Subject: {
+                Charset: 'UTF-8',
+                Data: 'HackerNews Best Stories',
+            },
+        },
+        Source: 'no-reply@hn500.xyz',
+    };
+
+    ses.sendEmail(params, function (err, data) {
+        if (err) console.log(err, err.stack);
+        else console.log(data);
+    });
 };
 
 run().catch(console.error);
